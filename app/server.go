eb92f237d9e4808de6f3e631c1893d19d7adb273
Wildchild <gustavo.romerorz@gmail.com>
2024-04-25 18:39:15 -0600

    support for 42069 port

--- app/server.go
+++ app/server.go
@@ -4,16 +4,19 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 
 	"github.com/codecrafters-io/redis-starter-go/app/services"
 )
 
 func main() {
-
 	server := NewServer()
 	server.Start()
-
 }
+
+const (
+	PORT = "--port"
+)
 
 type Server struct {
 	QChan    chan any
@@ -21,12 +24,25 @@ type Server struct {
 	rs       *services.RedisService
 }
 
+type serverOptions struct {
+	port int
+}
+
 func NewServer() *Server {
 	return &Server{connChan: make(chan net.Conn), rs: services.NewRedisService()}
 }
 
 func (s *Server) Start() {
-	listener, err := s.createConnection()
+
+	serverOps := buildServerOptions()
+	var listener net.Listener
+	var err error
+
+	if serverOps.port != 0 {
+		listener, err = s.createConnection(serverOps.port)
+	} else {
+		listener, err = s.createDefaultConnection()
+	}
 
 	if err != nil {
 		log.Fatal("Could not create connection" + err.Error())
@@ -37,16 +53,22 @@ func (s *Server) Start() {
 	go listen(listener, s)
 
 	<-s.QChan
-
 }
 
-func (s *Server) createConnection() (net.Listener, error) {
+func (s *Server) createDefaultConnection() (net.Listener, error) {
+	return s.createConnection(6379)
+}
 
-	log.Println("Accepting connections")
+func (s *Server) createConnection(port int) (net.Listener, error) {
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	portStr := strconv.Itoa(port)
+
+	log.Println("Accepting connections using port:" + portStr)
+
+	l, err := net.Listen("tcp", "0.0.0.0:"+portStr)
+
 	if err != nil {
-		log.Println("Failed to bind to port 6379")
+		log.Printf("Failed to bind to port %s, err: %s ", portStr, err)
 		os.Exit(1)
 	}
 
@@ -64,17 +86,41 @@ func listen(l net.Listener, s *Server) {
 		log.Println("connection", conn.RemoteAddr())
 
 		s.connChan <- conn
-
 	}
 }
 
 func handleConn(connChan chan net.Conn, rs *services.RedisService) {
 
 	for {
-
 		select {
 		case conn := <-connChan:
 			go rs.HandleConn(conn)
 		}
 	}
+}
+
+func buildServerOptions() serverOptions {
+	args := os.Args
+	ops := serverOptions{}
+
+	processedArgs := 0
+	for processedArgs < len(args) {
+		switch args[processedArgs] {
+		case PORT:
+			if processedArgs+1 >= len(args) {
+				log.Fatal("missing port argutmen")
+			}
+			processedArgs++
+			port, err := strconv.Atoi(args[processedArgs])
+			if err != nil {
+				log.Fatal("Could not get the port value: " + err.Error())
+			}
+
+			ops.port = port
+		default:
+			processedArgs++
+		}
+
+	}
+	return ops
 }
